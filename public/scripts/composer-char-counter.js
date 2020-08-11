2f03b1ccd35c5521ca2666505811e76a5ec4dcdd
Daniel Nascimento <danielhn@gmail.com>
2020-08-11 21:46:30 +0000

    Added function to count the characters and warn if they are over the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,32 @@
-$document().ready(function() {
+$(document).ready(function() {
   console.log("Working");
+});
+
+// i = 0;
+// $(document).ready(function() {
+//   $('textarea').keydown(function() {
+//     $('output').text(i +=1);
+//   });
+// });
+
+
+// $(document).ready(function() {
+//   $('textarea').focus(function () {
+//    console.log(this); 
+//   })
+// });
+
+$(document).ready(function() {
+  $('textarea').on('input', function () {
+    let maxChar = 140;
+    let numChar = $('#type-text').val().length;
+    let wordCount = maxChar - numChar;
+    const colorId = $('#char-count');
+    colorId.text(wordCount)
+    if (wordCount >= 0) {
+     colorId.removeClass('negative');
+    } else {
+      colorId.addClass('negative');
+    }
+  })
 });
