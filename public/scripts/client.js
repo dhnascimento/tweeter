dfc2a2553146114141bf9e7a306c662e8a9bdc70
Daniel Nascimento <danielhn@gmail.com>
2020-08-13 02:17:32 +0000

    Added escape function and refactored create tweet and post request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,67 +3,51 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
+
+// For example, with HTML, <script> would be converted to &lt;script&gt;
+// <script>$('body').empty()</script>
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// function encodeHTML(s) {
+//   return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+// };
 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 
 const renderTweets = function(tweets) {
-  let counter = 1;
-  for (const item of tweets ) {
-    let tag = `<article id=${counter.toString()}></article>`;
-    createTweetElement(item, tag, counter);
-    counter ++;
+  for (const item of tweets.reverse() ) {
+    createTweetElement(item);
   }
-}
+};
 
-const createTweetElement = function(tweet, tag, counter) {
+const createTweetElement = function(tweet) {
     let date1 = new Date(tweet.created_at);
     let date2 = new Date(Date.now());
     let milis = date2 - date1;
     let days = Math.floor((milis)/(1000 * 3600 * 24));
     
-    let html = `<header class="tweettop">
+    let html = `
+  <article>
+    <header class="tweettop">
       <div class=userinfo>
         <img class="avatar" src=${tweet.user.avatars}>
         <span class="username">${tweet.user.name}</span>
       </div>
       <h5 class="userurl">${tweet.user.handle}</h5>
     </header>
-    <p>${tweet.content.text}</p>
+    <p>${escape(tweet.content.text)}</p>
     <footer  class="tweetbottom">
     <span class="tweetinfo">${days.toString() + " days ago"}</span>
     <span class="tweetinfo">Cool icons here</span>
     </footer>
-    </article>`;
-
-  $('section').append(tag);
-  return $(`#${counter.toString()}`).append(html);
+  </article>`;
+  
+  return  $('.tweets-box').append(html);
  
 };
 
@@ -83,13 +67,16 @@ const urlTest = '/tweets';
 // Method 2;
 
 $(document).ready(function () { 
+  loadTweets();
   $('#tweet-form').on('submit', (evt) => {
   
     evt.preventDefault(); 
-    let dataT = $('textarea[name="text"]')
+    let dataT = $('#tweet-text');
+    // let dataT = $('textarea[name="text"]')
     let tweetLength = (dataT.val().length);
-    
-    if (tweetLength > 140) {
+    console.log(typeof dataT.val());
+    console.log(typeof tweetLength)
+;    if (tweetLength > 140) {
       return alert("Your tweet has more than 140 characters!")
     } else if (tweetLength === 0 || !dataT) {
       return alert("Please tweet something")
@@ -97,23 +84,23 @@ $(document).ready(function () {
       let newData= dataT.serialize();
       console.log('Button clicked, performing ajax call...');
       $.ajax({url: urlTest, data:newData, method: 'POST' })
-        .then((res) => {
-          console.log("yeeey", res);
-      });
+        .then((response) => {
+          $('textarea[name="text"]').val('');
+          loadTweets();
+      })
     }
   })
     
-    
-    
+});    
     const loadTweets = function() {
+      $('.tweets-box').empty();
       let dataJ = '/data-files/initial-tweets.json';
       console.log(dataJ);
-      $.ajax({url: '/tweets', method: `GET`})
+      $.ajax({url: '/tweets', method: 'GET'})
       .then((data) => {
         return renderTweets(data)
       }) 
     }
     
-    loadTweets();    
+    // loadTweets();    
     
-});
